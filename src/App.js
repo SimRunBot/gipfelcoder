c2c115387e95ee5092d45538d29bfee1c1c44982
simon <simon.r.both@gmail.com>
2021-03-20 05:50:34 +0100

    C1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 
 import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import ReactPlayer from 'react-player'
+import Video from "./data/vertical/Straight Wall (vertical).mp4";
+//Overhang (vertical + horizontal).mp4
+//Traverse (horizontal).mp4
 
 function App() {
+  const [time,setTime] = useState(0);
+  const [videoFilePath,setVideoFilePath] = useState(Video); 
+  
+
+  function syncTime(newTime){
+    setTime(newTime);
+  }
+  
+  function VideoPlayer(props) {
+    const PlayerRef = useRef();
+    console.log(PlayerRef.current);
+
+    return(
+      <>
+        <ReactPlayer 
+          url={props.video_file} 
+          ref={PlayerRef}
+          controls={true}/>
+      </>
+    );
+  }
+
   return (
     <div className="App">
+
+      <h1> Time: {time} </h1>
+
+      <VideoPlayer 
+        video_file={videoFilePath}
+        synchTime={syncTime}
+        />
       
+      <video 
+        src={Video}>
+      </video>
+
     </div>
   );
+
 }
 
 export default App;
